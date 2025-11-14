88bccb29041d9b11df4e24b049a46915e6b7f04c
brunodantas9 <brunoasouza68@gmail.com>
2025-11-14 10:52:09 -0300

    Add Professor interface to test_activity_unb.py

---
+++ tests/test_activity_unb.py
@@ -0,0 +1,12 @@
+export interface Professor {
+  id: string;
+  siape: string;
+  nome: string;
+  departamento: string;
+  campus: string;
+  fotoUrl: string;
+  email: string;
+  areasPesquisa: string[];
+  artigos?: any[];
+  disciplinas?: any[];
+}
