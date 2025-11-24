fc55ffa13826ed4c3de9b7082bdbd92692792443
brunodantas9 <brunoasouza68@gmail.com>
2025-11-24 17:26:55 -0300

    Correção pagina orientador

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,10 +1,11 @@
-
-
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'; 
 import App from '../App';
 import HomePage from '../pages/HomePage';
 import SobreNosPage from '../pages/SobreNosPage';
 import ProfessoresPage from '../pages/ProfessoresPage';
+// Importe as páginas que faltam
+import ProfessorDetailPage from '../pages/ProfessorDetailPage';
+import EncontrarOrientadorPage from '../pages/EncontrarOrientadorPage';
 
 const router = createBrowserRouter(
   [
@@ -14,7 +15,15 @@ const router = createBrowserRouter(
       children: [
         { path: '/', element: <HomePage /> },
         { path: '/professores', element: <ProfessoresPage /> },
+        
+        // ADICIONANDO A ROTA DE DETALHES (importante para /professores/:id funcionar oficialmente)
+        { path: '/professores/:id', element: <ProfessorDetailPage /> }, 
+        
         { path: '/sobre-nos', element: <SobreNosPage /> },
+        
+        // ADICIONANDO A ROTA QUE ESTAVA FALTANDO
+        { path: '/orientador', element: <EncontrarOrientadorPage /> }, 
+
         {
           path: '*',
           element: <Navigate to="/" replace />,
